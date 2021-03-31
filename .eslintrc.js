1dbe13e0c11d58d50a0ad8a1c99b63ea681f7201
Imamachi-n <naoto.imamachi@gmail.com>
2021-03-31 22:02:19 +0900

    chore: eslintやaws-cdkの基本設定を追加

---
+++ .eslintrc.js
@@ -0,0 +1,58 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+
+  plugins: ["@typescript-eslint", "prettier"],
+
+  extends: [
+    "eslint:recommended",
+
+    // JavaScript Standard Coding Style
+    "standard",
+
+    // eslint-plugin-import
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript", // for @typescript-eslint/parser
+
+    // @typescript-eslint
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+
+    // eslint-plugin-jest
+    "plugin:jest/recommended",
+
+    // Prettier: 一番最後に設定すること（ESLintがPrettierと競合しないように、ESLintの設定を最後に上書きするため）
+    "plugin:prettier/recommended",
+    "prettier",
+    "prettier/@typescript-eslint",
+    "prettier/standard",
+  ],
+
+  rules: {
+    // ESLint
+    "no-console": "off",
+  },
+
+  settings: {
+    // eslint-plugin-import
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+
+      // eslint-import-resolver-typescript
+      typescript: {
+        alwaysTryTypes: true, // import で `<root>@types` ディレクトリ直下の型解決を可能にする
+      },
+    },
+
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+  },
+};
